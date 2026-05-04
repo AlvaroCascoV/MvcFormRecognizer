98c427cd58f39cba6fe5804d2309ece8d34675ab
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-05-04 13:14:02 +0200

    Add project files.

---
+++ MvcFormRecognizer/Models/FacturaReconocida.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcFormRecognizer.Models
+{
+    public class FacturaReconocida
+    {
+        public string Proveedor { get; set; }
+        public string Fecha { get; set; }
+        public string NumeroFactura { get; set; }
+        public decimal? Total { get; set; }
+
+    }
+}
